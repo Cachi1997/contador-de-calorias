986339f1308fd048fead6ec1f0707b296c635f49
cachi1997 <cachigaliano1997@gmail.com>
2025-04-25 16:49:51 -0300

    Add delete functionality to ActivityList and update activity reducer

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { Activity, Category } from "../types";
 import categories from "../data/categories";
 import { useMemo } from "react";
-import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { ActivityActions } from "../reducers/activity-reducer";
 
 type ActivityListProps = {
@@ -47,6 +47,16 @@ const ActivityList = ({ activities, dispatch }: ActivityListProps) => {
               }
             >
               <PencilSquareIcon className="w-8 h-8 text-gray-800" />
+            </button>
+            <button
+              onClick={() =>
+                dispatch({
+                  type: "delete-activity",
+                  payload: { id: activity.id },
+                })
+              }
+            >
+              <XCircleIcon className="w-8 h-8 text-red-500" />
             </button>
           </div>
         </div>
