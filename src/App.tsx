f06918278cc2e47c303809ca2cc9ab4549f2d9cd
cachi1997 <cachigaliano1997@gmail.com>
2025-04-25 17:41:21 -0300

    Add CalorieTracker component as a new feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
+
+  const canRestartApp: boolean = state.activities.length > 0;
 
   return (
     <>
@@ -13,12 +19,22 @@ function App() {
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de calorías
           </h1>
+          <button
+            className="bg-white text-lime-600 cursor-pointer uppercase text-sm font-bold px-5 py-2 rounded-lg disabled:opacity-50"
+            disabled={!canRestartApp}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar app
+          </button>
         </div>
       </header>
       <section className="bg-lime-500 py-20 px-5">
         <div className="max-w-4xl mx-auto">
           <Form dispatch={dispatch} state={state} />
         </div>
+      </section>
+      <section className="bg-gray-800 py-10 ">
+        <div className="max-w-4xl mx-auto"></div>
       </section>
       <section className="p-10 mx-auto max-w-4xl">
         <ActivityList activities={state.activities} dispatch={dispatch} />
